9d608f344043f2bfbbeca9107c572fe607bda651
Oriah Ran <oriah.r@diglobal.tech>
2022-02-04 16:55:00 +0200

    setting index.js as main

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import style from "./Style.module.css"
 import ContentBox from './components/ContentBox/ContentBox';
 
 
-
 function App() {
   return (
     <div className="App" >
       <TimelineComponent className={style.contentWrapper}>
-        <ContentBox label='Feb 2019' >   Feb 2019  </ContentBox>
-        <ContentBox label='Feb 2019' type='text' mark={'2019'} className={style.dummyContent} />
+        <ContentBox label='Feb 2019' type={MARK_TYPES.TEXT} mark={'2019'} className={style.dummyContent}>Feb 2019</ContentBox>
 
-        <ContentBox label='Dec 2018'>Dec 2018</ContentBox>
-        <ContentBox label='Dec 2018' type={MARK_TYPES.BULLET} className={style.dummyContent} />
+        <ContentBox label='Dec 2018' type={MARK_TYPES.BULLET} className={style.dummyContent}>Dec 2018</ContentBox>
 
-        <ContentBox label='Jan 2018' >      Jan 2018   </ContentBox>
-        <ContentBox label='Jan 2018' type='text' mark={'2018'} className={style.dummyContent} />
+        <ContentBox label='Jan 2018' type={MARK_TYPES.TEXT} mark={'2018'} className={style.dummyContent}>Jan 2018 </ContentBox>
       </TimelineComponent>
     </div>
   );
