86de1dc83aa21738b6d6b31bf212d269c94c7bfa
Oriah Ran <oriah.r@diglobal.tech>
2022-02-08 21:24:07 +0200

    clean

--- src/lib/components/TimelineComponent/scripts/dataFunctions.js
+++ src/lib/components/TimelineComponent/scripts/dataFunctions.js
@@ -4,14 +4,10 @@ export const buildData = (contentRef, wrapperHeight) => {
     const scrollHeight = contentRef.offsetHeight
 
     let allMarkedNodes = contentRef.querySelectorAll("div[name='timeline-scroll']")
-    // let allMarkedNodes = contentRef.querySelectorAll("div[data-type*='timeline-scroll-']")
 
-    // let parentHeight = 0
     let dataList = _.reduce(allMarkedNodes, (acc, node) => {
-        // let { label, text, type } = node.dataset
         const offsetTop = node.offsetTop
         const offsetHeight = node.offsetHeight
-        // if (offsetTop + offsetHeight > parentHeight) parentHeight = offsetTop + offsetHeight
 
         acc.push({
             ...node.dataset,
